2b696c7093e0bc41522f5575b215a56db668135e
rickert156 <rickert156@rambler.ru>
2024-12-06 14:51:19 +0400

    new version

--- tools/parserCompanies.py
+++ tools/parserCompanies.py
@@ -13,15 +13,17 @@ def TitleCompany(driver):
     except:title_company = 'N/A'
     return title_company
 
+def SiteCompany(driver):
+    try:
+        domain_block = driver.find_element(By.CLASS_NAME, 'text-blue-600.ellipsis')
+        site = domain_block.find_element(By.TAG_NAME, 'a').text
+    except:site = 'N/A'
+    return site
+
 def AboutCompany(driver):
     try:about_company = driver.find_element(By.CSS_SELECTOR, 'div[class*="prose col-span-11"]').text
     except:about_company = 'N/A'
     return about_company
-
-def FounderComapny(driver):
-    try:founder_company = driver.find_element(By.CSS_SELECTOR, 'div[class*="flex flex-col"]').text.split('Founders')[1].strip()
-    except:founder_company = 'N/A'
-    return founder_company
 
 def SocialLink(driver):
     try:
@@ -37,11 +39,6 @@ def SocialLink(driver):
                 SOCIAL_LINK+=[link]
     except:name = 'N/A'
     return LIST_NAME, SOCIAL_LINK
-
-def TechCompany(driver):
-    try:tech_company = driver.find_element(By.CSS_SELECTOR, 'div[class*="prose col-span-11 mx-5"]').text
-    except:tech_company = 'N/A'
-    return tech_company
 
 def InfoCompany(driver):
     try:info_company = driver.find_element(By.CSS_SELECTOR, '"flex flex-wrap gap-1"')
@@ -77,42 +74,56 @@ def CollectInfo(driver, url):
     driver.get(url);time.sleep(3)
 
     company_name = CompanyName(driver)
+    site = SiteCompany(driver)
     title_company = TitleCompany(driver)
     about_company = AboutCompany(driver)
-    founder_company = FounderComapny(driver)
-    #tech_company = TechCompany(driver)
     social_names, link = SocialLink(driver)
     jobs_name, jobs_info, jobs_link = JobPosts(driver)
-    
+
     border = '-'*40
     print(url)
-    print(border)
-    print(f'{RED}Company Name:{RESET}\t{GREEN}{company_name}{RESET}')
-    print(border)
+    print(f'{RED}Company Name:{RESET}\t{GREEN}{company_name}{RESET} |  {YELLOW}{site}{RESET}')
     print(f'{RED}Company Title:{RESET}\t{GREEN}{title_company}{RESET}')
     print(border)
     print(f'{RED}About Company:{RESET}\t{GREEN}{about_company}{RESET}')
-    print(border)
-    print(f'{RED}Founders Company:{RESET}\n{GREEN}{founder_company}{RESET}')
     try:
         print(border)
         number_persone = 0
+        social_link = ''
         for social in social_names:
             linkedin = link[number_persone]
+            social_link = f'{social_link}{linkedin}\n'
             person_count = number_persone+1
-            print(f'{RED}Founder[{person_count}] {RESET}{GREEN}{social}:{RESET}\t{YELLOW}{linkedin}{RESET}')
+            print(f'{RED}Founder[{person_count}] {RESET}{GREEN}{social}{RESET}')
             number_persone+=1
-        print(border)
+        social_link = social_link.strip()
+        print(f'{social_link}\n{border}')
     except Exception as err:print(f'Error: {err}')
     try:
         number_post = 0
+        job = ''
         for job_name in jobs_name:
             job_info = jobs_info[number_post]
             job_link = jobs_link[number_post]
+            
             job_info = job_info.replace('\n', ' | ')
+            
+            job_experience = job_info.split('|')[-1].strip()
+            job_location = job_info.split('|')[0].strip()
+            job_rests = job_info.split('|')[1:-1]
+            job_rest = ''
+            for job in job_rests:
+                job_rest = f'{job_rest}{job} '
+            
             job_count = number_post+1
             if 'Interview Process' in job_name:job_name = job_name.split('Interview Process')[0].strip()
             print(f'{RED}Job Post [{job_count}]{RESET}\t{GREEN}{job_name}: {job_link}\n{job_info}{RESET}\n')
+            
+            with open('testing.csv', 'a+') as file:
+                write = csv.writer(file)
+                write.writerow([job_name, company_name, site, job_location, job_experience, job_rest, job_info, job_link, title_company, about_company, social_link])
+            
             number_post+=1
         print(border)
     except Exception as err:print(f'Error: {err}')
+
