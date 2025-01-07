fc1be41fbf13f5fb9e131fed0252d401a825509c
rickert156 <rickert156@rambler.ru>
2025-01-07 14:32:50 +0400

    Add hook slack

--- config.py
+++
@@ -1,4 +0,0 @@
-sender = 'rampa@thebrightspot.online'
-password = 'Jf3r#zt|3uO!'
-send_to = 'maksimnegulyaev@gmail.com'
-slack_hook = 'https://hooks.slack.com/services/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'
